70f222a16f95a54eb3eb8d15465bd0b5c2c61dc1
KoushikVijji <143944134+KoushikVijji@users.noreply.github.com>
2023-10-09 19:17:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css';
 
 function App() {
+  const colors = [
+    'red', 'greenyellow', 'blue', 'yellow', 'violet', 'aqua', 'orange', 'darkmagenta', 'pink',
+    'green', 'indianred', 'aquamarine', 'brown', 'orangered', 'cornflowerblue', 'gold'
+  ];
+
+  const [colorBoxesVisible, setColorBoxesVisible] = useState(false);
+  const [buttonBackgroundColor, setButtonBackgroundColor] = useState('rgb(70, 67, 67)');
+
+  const handleButtonClick = () => {
+    setColorBoxesVisible(!colorBoxesVisible);
+  };
+
+  const handleColorBoxClick = (color) => {
+    setButtonBackgroundColor(color);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='container'>
+      <h1>Color Picker</h1>
+      <div id='colors'>
+        {colorBoxesVisible &&
+          colors.map((color, index) => (
+            <div
+              key={index}
+              className='color-box'
+              style={{ backgroundColor: color }}
+              onClick={() => handleColorBoxClick(color)}
+            ></div>
+          ))}
+        <div className="break">
+          <button onClick={handleButtonClick}>Pick a color</button>
+        </div>
+      </div>
+      <style>
+        {`
+          #container #colors button {
+            border: none;
+            color: white;
+            background: ${buttonBackgroundColor};
+            font-size: 20px;
+            width: 150px;
+            height: 50px;
+            cursor: pointer;
+          }
+        `}
+      </style>
     </div>
   );
 }
